a513805f1bdfa89f7ebec5d73ffc3c627a25f57f
KJH <aljihoon@ajou.ac.kr>
2021-04-22 14:25:23 +0900

    로그인(토큰X)

---
+++ src/main/java/com/hawkeye/capstone/token/JwTokenProvider.java
@@ -0,0 +1,9 @@
+package com.hawkeye.capstone.token;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwTokenProvider {
+}
